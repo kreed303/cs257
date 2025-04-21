ebec843aa9e8ee0d1213d14d388ccc79e15dc3ca
Samuel Reiter <samreiter9@gmail.com>
2025-04-21 09:44:47 -0500

    updating the scraper

--- data/metaDataScraper.py
+++ data/metaDataScraper.py
@@ -1,36 +1,43 @@
 # Conda activate MusicApp
 
 import os
-from mutagen.mp3 import MP3  
+from mutagen.mp3 import MP3
 from mutagen.easyid3 import EasyID3  
+from mutagen.id3 import ID3
 import pandas as pd
 import pdb
 
-DIRECTORY = os.getcwd()
-print(DIRECTORY)
-album_csv = pd.DataFrame(columns=["album", "composer", "length", "title", "artist", "album artist", "organization", "tracknumber"," genre", "date"])
-
-for path, _, files in os.walk(DIRECTORY):
-    for name in files:
-        filename = os.path.join(path, name)
-        try:
+def writeTagsToCsv():
+    album_csv = pd.DataFrame(columns=["album", "composer", "length", "title", "artist", "albumartist", "organization", "tracknumber","genre", "date"])
+    for path, _, files in os.walk(DIRECTORY):
+        for name in files:
+            filename = os.path.join(path, name)
             if filename.endswith(".mp3"):
+                mp3data = []
                 mp3file = MP3(filename, ID3 = EasyID3)
-                print(mp3file.values())
-                # pdb.set_trace()
-                mp3data = [x[0].strip() for x in mp3file.values()]
-                #print(mp3data)
+                for item in album_csv.columns:
+                    if item in mp3file.keys():
+                        mp3data.append(mp3file[item][0].strip())
+                    elif item == "title":
+                        mp3data.append(name.split(".")[0].lstrip("012345679 "))
+                    else:
+                        mp3data.append("-")
                 album_csv = pd.concat([pd.DataFrame([mp3data], columns = album_csv.columns), album_csv], ignore_index = True)
-        except:
-            pass
-            # print(filename)
-# print(album_csv)
-album_csv.to_csv("songs.csv")
+            album_csv.to_csv("songs.csv")
 
-    # print(os.stat(filename))
-    # try:
-    #     with open(filename, "r") as f:
-    #         data = f.read()
-    #         print(data)
-    # except UnicodeDecodeError:
-    #     print("oops")
+def CreateEmptyTags():
+    for path, _, files in os.walk(DIRECTORY):
+        for name in files:
+            filename = os.path.join(path, name)       
+            if filename.endswith(".mp3"):
+                try:
+                    tags = ID3(filename)
+                except:
+                    tags = ID3()
+                tags.save(filename)
+
+if __name__ == "__main__":
+    DIRECTORY = os.getcwd()
+    print(DIRECTORY)
+    CreateEmptyTags()
+    writeTagsToCsv()
