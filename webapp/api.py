c8ccca109ae648141457afe065a9a847acea0ad2
Samuel Reiter <samreiter9@gmail.com>
2025-05-17 17:01:29 -0500

    messing with adding 'functions'

--- webapp/api.py
+++ webapp/api.py
@@ -17,34 +17,6 @@ from helperFunctions import *
 
 app = flask.Flask(__name__, static_folder='static', template_folder='templates')
 
-@app.route('/')
-def home():
-    '''
-    This is the home page!!!
-
-    input: none
-    output: a simple message saying hi!
-    
-    '''
-
-    return flask.render_template('home.html')
-
-@app.route('/artists')
-def temp():
-    '''
-    This is the home page!!!
-
-    input: none
-    output: a simple message saying hi!
-    
-    '''
-
-    return flask.render_template('artists.html')
-
-@app.route('/help')
-def help():
-    return flask.render_template('help.html')
-
 
 # 
 # ---------------------
@@ -255,6 +227,12 @@ def getSongs(shuffle=None):
     conn.close()
 
     return json.dumps(songs)
+
+
+# Other useful functions
+@app.route('/1.0/menu')
+def getMenuHTML():
+    return json.dumps("menu.html")
 
 
 # From Jeff's API code
