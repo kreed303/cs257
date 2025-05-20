c62f41c7b976c445b15a1c4cdd2bd7d83001e7f4
Katelyn Reed <katelyn.business111@gmail.com>
2025-05-20 13:59:46 -0400

    songs and albums working again

---
+++ webapp/static/albums.js
@@ -0,0 +1,34 @@
+import { createMenu, getAPIBaseURL } from './webapp.js';
+
+window.addEventListener("load", initialize);
+
+function initialize() {
+    createMenu();
+    addAlbumsTable();
+}
+
+function addAlbumsTable() {
+    var URL = getAPIBaseURL() + '/1.0/albums'
+
+    fetch(URL, {method: 'get'})
+
+    .then((response) => response.json())
+
+    .then(function(albums) {
+        var albumsList = document.getElementById('pageData');
+
+        var albumsHTML = '<table class="musicDataTable">';
+        for (var i=0; i<albums.length; i++) {
+            var album = albums[i]
+            albumsHTML += '<tr class="musicDataEntry"><td>' + album.albumName + '</td></tr>';
+        }
+
+        albumsHTML += '</table>';
+
+        albumsList.innerHTML = albumsHTML;
+    })
+
+    .catch(function(error) {
+        console.log(error);
+    });
+}
