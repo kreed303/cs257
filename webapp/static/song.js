24f3a68c567af862c1a22d01de3af49c5dff9ab0
Samuel Reiter <samreiter9@gmail.com>
2025-05-29 23:03:40 -0500

    blah

--- webapp/static/song.js
+++ webapp/static/song.js
@@ -18,22 +18,21 @@ function playSong(){
 
 function addSongData() {
     var URL = getAPIBaseURL() + '/1.0/songs/' + songID;
+
     fetch(URL, {method: 'get'})
-        .then((response) => response.json())
-        .then(function(songs) {
-            var songsList = document.getElementById('pageData');
+    .then((response) => response.json())
+    .then(function(song) {
+        var songList = document.getElementById('pageData');
+        var songHTML = '<table class="musicDataTable">';
+        song = song[0]
+        for (let i = 0; i < Object.keys(song).length; i++) {
+            var key = Object.keys(song)[i];
+            var value = song[key];
+            songHTML += '<tr><td class="musicDataEntry">' + key + ":    " + value + '</td></tr>';
+            console.log(key, value);
+        }
+        songHTML += '<table>';
+        songList.innerHTML = songHTML;
+    })
 
-            var songsHTML = '<table class="musicDataTable">';
-            for (var i=0; i<songs.length; i++) {
-                var song = songs[i]
-                songsHTML += '<tr><td class="musicDataEntry"> <a class="musicLink" href="/songs/' + song.songid + '">' + song.songname + '</a></td></tr>';
-            }
-
-            songsHTML += '</table>';
-            songsList.innerHTML = songsHTML;
-        })
-
-        .catch(function(error) {
-            console.log(error);
-        });
 }
