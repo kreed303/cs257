a109c6d7bad0754e927354dbaf589c07388a1a8c
Samuel Reiter <samreiter9@gmail.com>
2025-06-07 17:32:13 -0500

    reformatting song data page

--- webapp/static/song.js
+++ webapp/static/song.js
@@ -21,15 +21,35 @@ function addSongData() {
     fetch(URL, {method: 'get'})
     .then((response) => response.json())
     .then(function(song) {
-        var songList = document.getElementById('pageData');
-        var songHTML = '<table class="musicDataTable">';
         song = song[0]
-        for (let i = 0; i < Object.keys(song).length; i++) {
-            var key = Object.keys(song)[i];
-            var value = song[key];
-            songHTML += '<tr><td class="musicDataEntry">' + key + ":    " + value + '</td></tr>';
-            console.log(key, value);
-        }
+        var songList = document.getElementById('pageData');
+
+
+        var songHTML = '';
+        
+
+        var key = "songname"
+        songHTML += '<h2>' + song[key] + '</h2>';
+
+        songHTML += '<table class="musicDataTable">';
+        var key = "tracknumber"
+        songHTML += '<tr><td class="musicDataEntry"> Track Number:    ' + song[key] + '</td></tr>';
+
+        var key = "artistname"
+        songHTML += '<tr><td class="musicDataEntry"> Artist Name:    ' + song[key] + '</td></tr>';
+
+        var key = "albumname"
+        songHTML += '<tr><td class="musicDataEntry"> Album Name:    ' + song[key] + '</td></tr>';
+
+        var key = "songlength"
+        songHTML += '<tr><td class="musicDataEntry"> Song Length:    ' + Math.floor(song[key] / 60000) + ":" +  Math.floor((song[key] / 10000) % 60) + '</td></tr>';
+
+        // for (let i = 0; i < Object.keys(song).length; i++) {
+        //     var key = Object.keys(song)[i];
+        //     var value = song[key];
+        //     songHTML += '<tr><td class="musicDataEntry">' + key + ":    " + value + '</td></tr>';
+        //     console.log(key, value);
+        // }
         songHTML += '<table>';
         songList.innerHTML = songHTML;
     })
--- webapp/static/songs.js
+++ webapp/static/songs.js
@@ -45,7 +45,7 @@ function addSongsTable(shuffle) {
         }
 
         songsHTML += '</table>';
-        songsList.innerHTML = songsHTML;
+        songsList.innerHTML = songsHTML;    
     })
 
     .catch(function(error) {
